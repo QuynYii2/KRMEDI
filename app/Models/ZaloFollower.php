615752ffd60eacdf4788b417f517228a9df9841b
Egamorft <huytung.htbn@gmail.com>
2024-03-08 08:43:29 +0700

    fix softdelete

--- app/Models/ZaloFollower.php
+++ app/Models/ZaloFollower.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ZaloFollower extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $table = 'zalo_followers';
     protected $fillable = [
         'avatar',
