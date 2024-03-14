cf50a49d6eab2837419ec0492ee49f4c0a83bd4b
Egamorft <huytung.htbn@gmail.com>
2024-03-14 13:43:31 +0700

    fix import class

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\TypeProductCart;
+use App\Models\online_medicine\ProductMedicine;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
