0da67ad7160c9e571d891d6873389ba2044936b1
Egamorft <huytung.htbn@gmail.com>
2024-03-14 10:10:35 +0700

    fix orders response

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $table = 'orders';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'user_id',
+        'full_name',
+        'email',
+        'phone',
+        'address',
+        'total_price',
+        'shipping_price',
+        'discount_price',
+        'total',
+        'order_method',
+        'status',
+        'created_at',
+        'type_product',
+        'aha_order_id'
+    ];
 }
