47485720ca8b11900a608624479f4a33147bed13
datnguyen2001 <nguyendatdz01@gmail.com>
2024-05-27 12:23:57 +0700

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,17 +15,17 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-//        return $next($request)
-//            ->header('Access-Control-Allow-Origin', '*')
-//            ->header('Access-Control-Allow-Methods', '*')
-//            ->header('Access-Control-Allow-Credentials', true)
-//            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
-//            ->header('Accept', 'application/json');
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin' , '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');
+//        $response = $next($request);
+//        $response->headers->set('Access-Control-Allow-Origin' , '*');
+//        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+//        $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization');
 
-        return $response;
+//        return $response;
     }
 }
