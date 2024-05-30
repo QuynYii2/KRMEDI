0362c545717fb0ed07276e27215bab368f0f4b2c
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-05-30 09:23:23 +0700

    fix bug token 2

--- resources/views/admin/medical-results/create.blade.php
+++ resources/views/admin/medical-results/create.blade.php
@@ -83,10 +83,10 @@
         </div>
     </div>
     <script>
-        let accessToken = `Bearer ` + token;
-        let headers = {
-            "Authorization": accessToken
-        };
+        // let accessToken = `Bearer ` + token;
+        // let headers = {
+        //     "Authorization": accessToken
+        // };
 
         $(document).ready(function () {
             $(window).on('popstate', function () {
