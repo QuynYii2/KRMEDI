9d605c9299e3314c8c8e44d2abaffd952b3e610e
datnguyen2001 <nguyendatdz01@gmail.com>
2024-06-28 16:08:28 +0700

    view mobile

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,10 +5,9 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Fashi Template">
     <meta name="keywords" content="Fashi, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="zalo-platform-site-verification" content="RjwYAelHAo9i-9uSliWqHdtPltwHhsmbCZaq" />
