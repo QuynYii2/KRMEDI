e03da3892661d2c980ef96cd60d4cf8182cd100f
datnguyen2001 <nguyendatdz01@gmail.com>
2024-07-12 10:25:29 +0700

    edit nv

--- resources/views/chuyen-khoa/danh-sach-theo-chuyen-khoa.blade.php
+++ resources/views/chuyen-khoa/danh-sach-theo-chuyen-khoa.blade.php
@@ -587,7 +587,7 @@
                 clinics.forEach(function(clinic, index) {
                     var distance = distances[index];
                     var clinicElement = $('.specialList-clinics[data-clinic-id="' + clinic.id + '"]');
-                    if (distance <= 10) {
+                    if (distance <= 100) {
                         clinicElement.attr('data-marker-index', clinicIndex++);
                     } else {
                         clinicElement.hide();
@@ -619,7 +619,7 @@
                 results.forEach(function(result) {
                     var distance = result.distance;
                     var pharmacyElement = result.pharmacyElement;
-                    if (distance <= 10) {
+                    if (distance <= 100) {
                         pharmacyElement.attr('data-marker-index', pharmacyIndex++);
                     } else {
                         pharmacyElement.hide();
