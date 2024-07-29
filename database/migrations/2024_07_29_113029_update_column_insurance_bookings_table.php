1cd73c3dd1a850e121eb5e4deb37747c4074b325
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-07-29 15:39:05 +0700

    migrate

---
+++ database/migrations/2024_07_29_113029_update_column_insurance_bookings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->string('insurance_use')->after('extend')->nullable();
+            $table->string('insurance_family_id')->after('insurance_use')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn(['insurance_use', 'insurance_family_id']);
+        });
+    }
+};
