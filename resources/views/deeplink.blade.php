31598082516146b1c200331a5d4189520668ec82
datnguyen2001 <nguyendatdz01@gmail.com>
2024-10-07 09:43:37 +0700

    deeplink

---
+++ resources/views/deeplink.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body { font-family: Arial, sans-serif; background-color: #f4f4f4; text-align: center; margin: 0; }
+        h1 { color: #333; margin-top: 50px; }
+        p { color: red; }
+        a { text-decoration: none; }
+        img { width: 150px; margin: 20px; }
+        .line-link{display: flex;align-items: center;justify-content: center;}
+    </style>
+</head>
+<body>
+<img src="https://krmedi.vn/img/icons_logo/logo-new.png" alt="">
+<h1>Ứng dụng Krmedi hiện không hỗ trợ trên web.</h1>
+<p>Vui lòng tải ứng dụng từ</p>
+<div class="line-link">
+    <a href="#" target="_blank"><img src="https://krmedi.vn/img/icon-app-android.png" alt="Play Store"></a>
+    <a href="https://apps.apple.com/vn/app/krmedi/id6476542829?l=vi" target="_blank">
+        <img src="https://krmedi.vn/img/icon-app-ios.png" alt="App Store"></a>
+</div>
+</body>
+</html>
