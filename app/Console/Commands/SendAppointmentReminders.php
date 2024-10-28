162d3f5cb0bbdb67ce0dd916fdda66e4f891d779
datnguyen2001 <nguyendatdz01@gmail.com>
2024-10-28 09:12:17 +0700

    noti

--- app/Console/Commands/SendAppointmentReminders.php
+++ app/Console/Commands/SendAppointmentReminders.php
@@ -29,73 +29,76 @@ class SendAppointmentReminders extends Command
      */
     public function handle()
     {
-        $upcomingAppointments = Booking::where('check_in', '>=', Carbon::now()->addHours(2))
-            ->where('check_in', '<=', Carbon::now()->addHours(2)->endOfMinute())
-            ->get();
+        $upcomingAppointments = Booking::where('check_in', '=', Carbon::now()->addHours(2)->startOfMinute())->get();
 
-        foreach ($upcomingAppointments as $appointment) {
-            $user = User::find($appointment->user_id);
-            if ($user && $user->token_firebase) {
-                $notificationPayload = [
-                    'title' => 'Thông báo sắp đến giờ khám',
-                    'body' => 'Bạn có một lịch khám sắp diễn ra.',
-                ];
+        if (count($upcomingAppointments)>0){
+            foreach ($upcomingAppointments as $appointment) {
+                $user = User::find($appointment->user_id);
+                if ($user && $user->token_firebase) {
+                    $notificationPayload = [
+                        'title' => 'Thông báo sắp đến giờ khám',
+                        'body' => 'Bạn có một lịch khám sắp diễn ra.',
+                    ];
 
-                $data = [
-                    'title' => "Thông báo sắp đến giờ khám",
-                    'sender' => "",
-                    'url' => "#",
-                    'description' => "Bạn có một lịch khám sắp diễn ra.",
-                    'id' => '1',
-                    'routeKey'=>'/med-appointment-screen',
-                    'arguments'=>json_encode($appointment),
-                ];
+                    $data = [
+                        'title' => "Thông báo sắp đến giờ khám",
+                        'sender' => "",
+                        'url' => "#",
+                        'description' => "Bạn có một lịch khám sắp diễn ra.",
+                        'id' => '1',
+                        'routeKey'=>'/med-appointment-screen',
+                        'arguments'=>json_encode($appointment),
+                    ];
 
-                $androidPayload = [
-                    'notification' => [
-                        'icon' => 'ic_launcher',
-                        'channel_id' => 'default_channel_id',
-                        'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
-                        'sound' => 'default',
-                    ],
-                ];
+                    $androidPayload = [
+                        'notification' => [
+                            'icon' => 'ic_launcher',
+                            'channel_id' => 'default_channel_id',
+                            'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
+                            'sound' => 'default',
+                        ],
+                    ];
 
-                $iosPayload = [
-                    'aps' => [
-                        'sound' => 'default',
-                        'badge' => 1,
-                    ],
-                ];
+                    $iosPayload = [
+                        'aps' => [
+                            'sound' => 'default',
+                            'badge' => 1,
+                        ],
+                    ];
 
-                $payload = [
-                    'token' => $user->token_firebase,
-                    'notification' => $notificationPayload,
-                    'data' => array_merge($data, [
-                        'channel_id' => 'default_channel_id',
-                    ]),
-                ];
+                    $payload = [
+                        'token' => $user->token_firebase,
+                        'notification' => $notificationPayload,
+                        'data' => array_merge($data, [
+                            'channel_id' => 'default_channel_id',
+                        ]),
+                    ];
 
-                $platform = $notification->platform ?? 'ANDROID';
+                    $platform = $notification->platform ?? 'ANDROID';
 
-                if ($platform === 'ANDROID') {
-                    $payload['android'] = $androidPayload;
-                } elseif ($platform === 'IOS') {
-                    $payload['apns'] = ['payload' => $iosPayload];
+                    if ($platform === 'ANDROID') {
+                        $payload['android'] = $androidPayload;
+                    } elseif ($platform === 'IOS') {
+                        $payload['apns'] = ['payload' => $iosPayload];
+                    }
+
+                    $response = FcmService::init()->request($payload);
+                    $responseBody = json_decode($response, true);
+
+                    if (isset($responseBody['name'])) {
+                        // Success
+                        $this->info("Notification sent successfully to user {$user->name} (User ID: {$user->id}).");
+                    } else {
+                        // Failure
+                        $this->error("Failed to send notification to user {$user->name} (User ID: {$user->id}).");
+                    }
+                }else{
+                    $this->error("User not found");
                 }
-
-                $response = FcmService::init()->request($payload);
-                $responseBody = json_decode($response, true);
-
-                if (isset($responseBody['name'])) {
-                    // Success
-                    $this->info("Notification sent successfully to user {$user->name} (User ID: {$user->id}).");
-                } else {
-                    // Failure
-                    $this->error("Failed to send notification to user {$user->name} (User ID: {$user->id}).");
-                }
-            }else{
-                $this->error("User not found");
             }
+        }else{
+            $this->error("No data");
         }
+
     }
 }
